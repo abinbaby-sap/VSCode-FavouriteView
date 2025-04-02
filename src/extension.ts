0ecdf3a061900d26322c9e65e667072c85d1c128
Abin Baby <51697829+abinbaby-sap@users.noreply.github.com>
2025-04-02 19:58:45 +0530

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,26 @@
 import * as vscode from 'vscode';
-import { FavouritesViewProvider, FavouriteItem } from './favouritesView';
+import { FavouriteItem, FavouritesViewProvider } from './favouritesView';
 
 export function activate(context: vscode.ExtensionContext) {
-  const favouritesViewProvider = new FavouritesViewProvider();
-  vscode.window.registerTreeDataProvider('favouritesView', favouritesViewProvider);
+	// register the tree view provider
+	const favouritesViewProvider = new FavouritesViewProvider();
+	vscode.window.registerTreeDataProvider('favouritesView', favouritesViewProvider);
 
-  context.subscriptions.push(vscode.commands.registerCommand('favourites.refresh', () => favouritesViewProvider.refresh()));
-  context.subscriptions.push(vscode.commands.registerCommand('favourites.openFavourite', (favoutiteItem: FavouriteItem) => {
-    vscode.window.showTextDocument(favoutiteItem.resourceUri);
-  }));
-  context.subscriptions.push(vscode.commands.registerCommand('favourites.addFavourite', (resourceUri: vscode.Uri) => {
-    vscode.window.showInputBox({ prompt: 'Enter a name for the favourite' }).then(label => {
-      if (label) {
-        favouritesViewProvider.addFavourite(label, resourceUri);
-        vscode.window.showInformationMessage(`Favourite ${label} added`);
-      }else{
-        vscode.window.showErrorMessage('Label is required');
-      };
-    });
-  }));
+	// register a new command to add a favourite file to the Favourites view
+	context.subscriptions.push(vscode.commands.registerCommand('favourites.addFavourite', (resourceUri: vscode.Uri) => {
+		vscode.window.showInputBox({ prompt: 'Enter a name for the favourite' }).then(label => {
+			if (label) {
+				favouritesViewProvider.addFavourite(label, resourceUri);
+				vscode.window.showInformationMessage(`Favourite ${label} added`);
+			} else {
+				vscode.window.showErrorMessage('Label is required');
+			};
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('favourites.openFavourite', (favoutiteItem: FavouriteItem) => {
+        vscode.window.showTextDocument(favoutiteItem.resourceUri);
+    }));
 }
 
-export function deactivate() {}
+export function deactivate() { }
